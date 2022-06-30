efe50fa68e29a3282a440389c50200395973cded
nimisha2502 <106615712+nimisha2502@users.noreply.github.com>
2022-06-30 23:47:20 +0530

    Create cricket.c

---
+++ cricket.c
@@ -0,0 +1,33 @@
+#include"declaration.h"
+#include"welcome.h"
+#include"menu.h"
+#include"newscoresheet.h"
+int main()
+{
+    int key;
+    char b;
+    File *fnew;
+    char lastinput;
+    while(TRUE)
+    {
+        welcome();
+        system("cls");
+        menu();
+    ch=getch();
+  char (ch);
+  switch(ch)
+  {
+      case 1;
+      fileopen(ch);
+      
+      case 2;
+      fileopen(ch);
+      getch();
+      
+      case 3;
+      system("cls");
+      exit(0);
+      
+  }
+}
+}  
